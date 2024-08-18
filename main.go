b8295d6fb76c2da8ee2ed33fa9169fcfb95099e7
ratulbasak <ratulbasak93@gmail.com>
2024-08-18 18:56:43 +0200

    added router hello

--- main.go
+++ main.go
@@ -9,16 +9,12 @@ import (
 	"simply-go/service"
 )
 
-// type hello struct {
-// 	Message string `json:"message"`
-// }
-
 func main() {
 	cfg := config.LoadConfig()
 
 	helloService := &service.HelloService{}
 	mux := router.SetupRouter(helloService)
-	fmt.Println("Server started on localhost:8080")
+	fmt.Println("Server started on ", cfg.Port)
 	err := http.ListenAndServe(cfg.Port, mux)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
